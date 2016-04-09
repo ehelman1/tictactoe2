ccfb80f98b4875c0857fd042be86df383d4b1b36
Edward Helman <eddhelman@gmail.com>
2016-04-09 11:10:28 -0400

    array with nine elements

--- test_tictactoe.rb
+++ test_tictactoe.rb
@@ -1,9 +1,11 @@
-require "test/minitest"
-require_relative "tictactoe.rb
+require 'minitest/autorun'
+require_relative "tictactoe.rb"
 
-class TestTictactoe < Minitest:: Test 
+class TestTictactoe <Minitest::Test 
 
-
+    def test_array_of_board_is_there
+        assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9], game_board)
+    end
 
 
 
---
+++ tictactoe.rb
@@ -0,0 +1,4 @@
+def game_board()
+    board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	 
+end
