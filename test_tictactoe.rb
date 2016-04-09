b86fb3e1a30428a79daa1368b9ed96a1d9a22345
Edward Helman <eddhelman@gmail.com>
2016-04-09 13:00:45 -0400

    update board with one marker

--- test_tictactoe.rb
+++ test_tictactoe.rb
@@ -4,9 +4,14 @@ require_relative "tictactoe.rb"
 class TestTictactoe <Minitest::Test 
 
     def test_array_of_board_is_there
-        assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9], game_board)
+        assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9], create_new_board)
     end
-
+	
+	def test_board_is_updated_for_player_1
+		assert_equal([1, 2, 3, 4, "X", 6, 7, 8, 9], update_board([1, 2, 3, 4, 5, 6, 7, 8, 9],4, "X"))
+		assert_equal([1, "X", 3, 4, 5, 6, 7, 8, 9], update_board([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, "X"))
+		assert_equal([1, 2, 3, 4, 5, 6, "O", 8, 9], update_board([1, 2, 3, 4, 5, 6, 7, 8, 9], 6, "O"))
+	end
 
 
 
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,9 @@
-def game_board()
+#takes no input and outpt is an array
+def create_new_board()
     board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	 
+end
+#take input for x and output is the string x in place of 5
+def update_board(board, position, marker)
+	board[position] = marker
+	board	
 end
