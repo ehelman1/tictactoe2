44a50b4c749e44fc9e65830fa46b6a6833806a82
Edward Helman <eddhelman@gmail.com>
2016-04-16 14:09:28 -0400

    refactor

--- test_tictactoe_game.rb
+++ test_tictactoe_game.rb
@@ -4,8 +4,8 @@ require_relative "tictactoe_game.rb"
 class TicTacToeGame < Minitest::Test
 
 	def test_play_game_is_over
-		player_1 = {:player_mode => Sequential.new  }
-		player_2 = {:player_mode => Random.new }
+		player_1 = {:player_mode => Sequential.new, :marker => "X"  }
+		player_2 = {:player_mode => Random.new,  :marker => "O" }
 		board = create_new_board
 		final_board = play_game(player_1, player_2, board)
 		assert_equal(true, game_over?(final_board, "X") || game_over?(final_board, "O"))
--- tictactoe_game.rb
+++ tictactoe_game.rb
@@ -2,17 +2,16 @@ require_relative "pick_ai.rb"
 require_relative "easy_ai.rb"
 
 def play_game(player_1, player_2, board)
-	
-	until game_over?(board, "X") == true || game_over?(board, "O") == true do
-		move1 = player_1[:player_mode].get_move(board)
-		board = update_board(board, move1 - 1, "X")
-		break if game_over?(board, "X") == true || game_over?(board, "O") == true
-		move2 = player_2[:player_mode].get_move(board)
-		board = update_board(board, move2 - 1, "O")
-	end
-	board
-	
+	move = player_1[:player_mode].get_move(board)
+	board = update_board(board, move - 1, player_1[:marker])
+		if game_over?(board, player_1[:marker])
+			board
+		else
+			play_game(player_2, player_1, board)
+		end
 end
+	
+	
 
 
 
